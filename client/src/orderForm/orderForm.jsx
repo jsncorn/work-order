4a870022453c44510d5c3bb7837e2491570e6437
jsncorn <jsncourn@gmail.com>
2022-03-08 22:14:46 -0800

    removed unnecessary imports

---
+++ client/src/orderForm/orderForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Orderform = () => {
+    return (
+        <div>
+
+        <h3>Create New Order</h3>
+        
+
+        </div>
+    );
+}
+
+export default Orderform;
