9582adb6e9d7bb6086b1449f679ff1d1045ea45c
jsncorn <jsncourn@gmail.com>
2022-03-08 23:34:29 -0800

    added customer information

--- client/src/components/orderForm/orderForm.jsx
+++ client/src/components/orderForm/orderForm.jsx
@@ -13,51 +13,80 @@ const Orderform = () => {
             <h3>Create New Order</h3>
             <form className='flex-row justify-center justify-space-between-md align-center' onSubmit={ handleFormSubmit }>
                 <div className='left'>
+
                     <div className='carDiv'>
+                    <h3 style={{width: '100%'}}>Car Information</h3>
                         <div className='carLeft'>
                             <div className='col-12'>
                                 <label>Year:
-                                    <input name="carYear" placeholder="car year" />
-                                    </label>
+                                    <input name="carYear" />
+                                </label>
                             </div>
                             <div className='col-12'>
                                 <label>Make:
-                                <input name="carMake" placeholder="car make" />
-                            </label>
+                                    <input name="carMake" />
+                                </label>
                             </div>
                             <div className='col-12'>
                                 <label>Model:
-                                <input name="carModel" placeholder="car model" />
-                            </label>
+                                    <input name="carModel" />
+                                </label>
                             </div>
                         </div>
                         <div className='carRight'>
                             <div className='col-12'>
                                 <label>Color:
-                                <input name="carColor" placeholder="car color" />
-                           </label>
+                                    <input name="carColor" />
+                                </label>
                             </div>
                             <div className='col-12'>
                                 <label>License Plate #:
-                                <input name="carPlate" placeholder="car license plate" />
-                            </label>
+                                    <input name="carPlate" />
+                                </label>
                             </div>
                             <div className='col-12'>
                                 <label>VIN #:
-                                <input name="carVin" placeholder="car vin" />
-                            </label>
+                                    <input name="carVin" />
+                                </label>
                             </div>
                         </div>
                     </div>
+
                     <div className='customerDiv'>
-                        <div className='col-12'>
-                            <input name="customerName" placeholder="customer name" />
+                    <h3 style={{width: '100%'}}>Customer Information</h3>
+                        <div className='customerLeft'>
+                            <div className='col-12'>
+                                <label>Name:
+                                    <input name="customerName" />
+                                </label>
+                            </div>
+                            <div className='col-12'>
+                                <label>Number:
+                                    <input name="customerNum" />
+                                </label>
+                            </div>
                         </div>
-                        <div className='col-12'>
-                            <input name="customerNum" placeholder="customer number" />
-                        </div>
-                        <div className='col-12'>
-                            <input name="customerAdd" placeholder="customer address" />
+                        <div className='customerRight'>
+                            <div className='col-12'>
+                                <label>Street
+                                    <input name="customerSt" />
+                                </label>
+                            </div>
+                            <div className='col-12'>
+                                <label>City
+                                    <input name="customerCity" />
+                                </label>
+                            </div>
+                            <div className='col-12'>
+                                <label>State
+                                    <input name="customerState" />
+                                </label>
+                            </div>
+                            <div className='col-12'>
+                                <label>ZIP
+                                    <input name="customerZip" />
+                                </label>
+                            </div>
                         </div>
                     </div>
                     <div className='col-12'>
