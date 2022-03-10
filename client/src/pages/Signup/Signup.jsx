c29434cb44f01bcf45a6160232d4e1845127157b
jsncorn <jsncourn@gmail.com>
2022-03-10 11:50:31 -0800

    added add employee

--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 import {useMutation} from '@apollo/client';
 import Auth from '../../utils/Auth';
+import { ADD_EMPLOYEE } from '../../utils/mutations';
 
 
 export default function Signup(props) {
 
   const [formState, setFormState ] = useState({email: '', password: ''});
-  const [addEmployee] = useMutation();
+  const [addEmployee] = useMutation(ADD_EMPLOYEE);
 
 
   return (
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,35 @@ mutation addOrder($carYear: Int!, $carMake: String!, $carModel: String!, $carCol
     }
 }
 `;
+
+export const LOGIN = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        employee {
+            _id
+        }
+    }
+}
+`;
+
+export const ADD_EMPLOYEE = gql`
+    mutation addEmployee(
+        $firstName: String!
+        $lastName: String!
+        $email: String!
+        $password: String!
+    ) {
+        addUser(
+            firstName: $firstName
+            lastName: $lastName
+            email: $email
+            password: $password
+        ) {
+            token
+            employee {
+                _id
+            }
+        }
+    }
+`;
