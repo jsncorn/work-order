4afa69b21bf9a463870e1ab1f1a4211b400e062b
jsncorn <jsncourn@gmail.com>
2022-03-10 11:40:13 -0800

    added users into tpyedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,6 +26,19 @@ type Query {
     order(orderId: ID!): Order
 }
 
+type Employee {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    order: [{Order.custName}]
+}
+
+type Auth {
+    token: ID
+    employee: Employee
+}
+
 type Mutation {
     addOrder(
         carYear: Int!,
@@ -44,7 +57,40 @@ type Mutation {
         orderEst: Int!,
     ): Order
 
+    updateOrder(
+        carYear: Int,
+        carMake: String,
+        carModel: String,
+        carColor: String,
+        carPlate: String,
+        carVin: String,
+        custName: String,
+        custNumber: String,
+        custSt: String,
+        custCity: String,
+        custState: String,
+        custZip: Int,
+        orderSum: String,
+        orderEst: Int,
+    ): Order
+
     removeOrder(orderId: ID!): Order
+
+    addEmployee(
+        firstName: String!
+        lastName: String!
+        email: String!
+        password: String!
+    ): Auth
+
+    updateUser(
+        firstName: String
+        lastName: String
+        email: String
+        password: String
+    ): User
+
+    login(email: String!, password: String!): Auth
 }
 
 
