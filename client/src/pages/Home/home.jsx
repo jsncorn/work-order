d00d8ba0ef4ad255e3305a54adf1876837ed09a0
jsncorn <jsncourn@gmail.com>
2022-03-09 20:33:57 -0800

    removed unnecessary stuff

--- client/src/pages/Home/home.jsx
+++ client/src/pages/Home/home.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import OrderList from '../../components/orderList/orderList';
-import Topbar from '../../components/topBar/topBar';
-import { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_ORDERS } from '../../utils/queries';
 
-function Home() {
+const Home = () => {
     
 const {loading, data} = useQuery(QUERY_ORDERS);
 const orders = data?.orders || [];
 
   return (
-      
     <div>
         <OrderList orders={orders} />
     </div>
