ede2d62b7c0413e91a36c75d826974d8bfbf10de
jsncorn <jsncourn@gmail.com>
2022-03-10 11:50:08 -0800

    added mutations

--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import {useMutation} from '@apollo/client';
 import Auth from '../../utils/Auth';
 
+
 export default function Signup(props) {
 
   const [formState, setFormState ] = useState({email: '', password: ''});
