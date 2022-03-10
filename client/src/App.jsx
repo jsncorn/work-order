e59c337a937720efcb1220b6b527db9334d142e9
jsncorn <jsncourn@gmail.com>
2022-03-09 19:34:46 -0800

    package change, orderform path to pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,35 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.scss';
-import OrderForm from './components/orderForm/orderForm';
+import OrderForm from './pages/orderForm/orderForm';
 import Topbar from './components/topBar/topBar';
 
 import { useState } from 'react';
 
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
 
-  const [ menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="App">
-      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-      <OrderForm />
-    </div>
+    <ApolloProvider client={ client }>
+      <Router>
+        <div className="App">
+          <Topbar menuOpen={ menuOpen } setMenuOpen={ setMenuOpen } />
+          <Routes>
+            <Route path='/newform' element={<OrderForm />}/>
+          </Routes>
+        </div>
+      </Router>
+
+    </ApolloProvider>
   );
 }
 
