5ad846efb54ec98966eebafef1fb9f843ef2000f
jsncorn <jsncourn@gmail.com>
2022-03-10 11:59:54 -0800

    added signup to app.jsc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import './App.scss';
 import OrderForm from './pages/orderForm/orderForm';
 import Topbar from './components/topBar/topBar';
 import Home from './pages/Home/Home';
+import Signup from './pages/Signup/Signup';
 
 import { useState } from 'react';
 import SingleOrder from './pages/singleOrder/singleOrder';
@@ -29,8 +30,8 @@ function App() {
             <Routes>
               <Route path='/' element={ <Home /> } />
               <Route path='/newform' element={ <OrderForm /> } />
-              <Route path="/orders/:orderId" element={<SingleOrder />} 
-              />
+              <Route path="/orders/:orderId" element={<SingleOrder />} />
+              <Route path='/signup' element={<Signup/>} />  
             </Routes>
           </div>
         </div>
