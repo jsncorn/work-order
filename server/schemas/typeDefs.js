1bda2108ae98a80629142726d002bee259e0dc7d
jsncorn <jsncourn@gmail.com>
2022-03-09 18:11:34 -0800

    added typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,52 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type Order {
+    _id: ID
+    carYear: Int
+    carMake: String
+    carModel: String
+    carColor: String
+    carPlate: String
+    carVin: String
+    custName: String
+    custNumber: Int
+    custSt: String
+    custCity: String
+    custState: String
+    custZip: Int
+    orderSum: String
+    orderEst: Int
+    doneBy: String
+}
+
+type Query {
+    orders: [Order]!
+    order(orderId: ID!): Order
+}
+
+type Mutation {
+    addOrder(
+        carYear: Int!,
+        carMake: String!,
+        carModel: String!,
+        carColor: String!,
+        carPlate: String!,
+        carVin: String!,
+        custName: String!,
+        custNumber: Int!,
+        custSt: String!,
+        custCity: String!,
+        custState: String!,
+        custZip: Int!,
+        orderSum: String!,
+        orderEst: Int!,
+        doneBy: String!,
+    ): Order
+
+    removeOrder(orderId: ID!): Order
+}
+
 
 `
 
