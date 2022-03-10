323fd6428cdcc8bc031f91eefcb1e5380173893e
jsncorn <jsncourn@gmail.com>
2022-03-10 12:59:30 -0800

    format

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,15 +1,27 @@
-const { AuthenticationError } = require('apollo-server-express');
 const {
-    Order, Employee
+    AuthenticationError
+} = require('apollo-server-express');
+const {
+    Order,
+    Employee
 } = require('../models');
+const {
+    signToken
+} = require('../utils/auth')
 
 const resolvers = {
     Query: {
         orders: async () => {
-            return Order.find().sort({ carYear: -1 });
+            return Order.find().sort({
+                carYear: -1
+            });
         },
-        order: async (parent, { orderId }) => {
-            return Order.findOne({ _id: orderId });
+        order: async (parent, {
+            orderId
+        }) => {
+            return Order.findOne({
+                _id: orderId
+            });
         },
         employee: async (parent, args, context) => {
             if (context.employee) {
@@ -22,41 +34,86 @@ const resolvers = {
         }
     },
     Mutation: {
-        addOrder: async (parent, { carYear, carMake, carModel, carColor, carPlate, carVin, custName, custNumber, custSt, custCity, custState, custZip, orderSum, orderEst  }) => {
-             return Order.create({ carYear, carMake, carModel, carColor, carPlate, carVin, custName, custNumber, custSt, custCity, custState, custZip, orderSum, orderEst });
-            },
-        removeOrder: async (parent, { orderId }) => {
-             return Order.findOneAndDelete({ _id: orderId });
-         },
-         addEmployee: async (parent, args) => {
-             const employee = await Employee.create(args);
-             const token = signToken(employee);
-             return {token, employee};
-         },
-         updateEmployee: async(parent, args, context) => {
-             if (context.employee) {
-                 return Employee.findByIdAndUpdate(context.employee.id, args, {
-                     new: true
-                 })
-             }
-             throw new AuthenticationError('not logged in')
-         },
-         login: async(parent, { email, password }) => {
-             const employee = await Employee.findOne({ email });
+        addOrder: async (parent, {
+            carYear,
+            carMake,
+            carModel,
+            carColor,
+            carPlate,
+            carVin,
+            custName,
+            custNumber,
+            custSt,
+            custCity,
+            custState,
+            custZip,
+            orderSum,
+            orderEst
+        }) => {
+            return Order.create({
+                carYear,
+                carMake,
+                carModel,
+                carColor,
+                carPlate,
+                carVin,
+                custName,
+                custNumber,
+                custSt,
+                custCity,
+                custState,
+                custZip,
+                orderSum,
+                orderEst
+            });
+        },
+        removeOrder: async (parent, {
+            orderId
+        }) => {
+            return Order.findOneAndDelete({
+                _id: orderId
+            });
+        },
+        addEmployee: async (parent, args) => {
+            const employee = await Employee.create(args);
+            const token = signToken(employee);
+            return {
+                token,
+                employee
+            };
+        },
+        updateEmployee: async (parent, args, context) => {
+            if (context.employee) {
+                return Employee.findByIdAndUpdate(context.employee.id, args, {
+                    new: true
+                })
+            }
+            throw new AuthenticationError('not logged in')
+        },
+        login: async (parent, {
+            email,
+            password
+        }) => {
+            const employee = await Employee.findOne({
+                email
+            });
 
-             if(!user) {
-                 throw new AuthenticationError('wrong login info');
-             }
+            if (!employee) {
+                throw new AuthenticationError('wrong login info');
+            }
 
-             const correctPw = await employee.isCorrectPassword(password);
-             if (!correctPw) {
-                 throw new AuthenticationError('wrong password');
-             }
+            const correctPw = await employee.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('wrong password');
+            }
 
-             const token = signToken(employee);
-             return {token, user};
+            const token = signToken(employee);
+            return {
+                token,
+                employee
+            };
 
-         }
+        }
     }
 }
 
