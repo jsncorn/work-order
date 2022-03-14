55f63b887b742b3b66295a620bf1a9beebcb0d2b
jsncorn <jsncourn@gmail.com>
2022-03-14 15:01:59 -0700

    form disappears when successfully sent

--- client/src/pages/orderForm/orderForm.jsx
+++ client/src/pages/orderForm/orderForm.jsx
@@ -3,64 +3,73 @@ import './orderForm.scss'
 import { ADD_ORDER } from '../../utils/mutations';
 import { useMutation } from '@apollo/client';
 import Auth from '../../utils/Auth';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Orderform(props) {
 
-    const [formState, setFormState] = useState({ carYear: '', carMake: '',
-carModel: '', carColor: '', carPlate: '', carVin: '', customerName: '',
-customerNum: '', customerSt: '', customerCity: '', customerState: '', customerZip:'',
-workSum: '', workEst: ''})
-    const [ addOrder ] = useMutation(ADD_ORDER);
-    const [formMsg, setFormMsg ] = useState('');
+    const [formState, setFormState] = useState({
+        carYear: '', carMake: '',
+        carModel: '', carColor: '', carPlate: '', carVin: '', customerName: '',
+        customerNum: '', customerSt: '', customerCity: '', customerState: '', customerZip: '',
+        workSum: '', workEst: ''
+    })
+    const [addOrder] = useMutation(ADD_ORDER);
+    const [formMsg, setFormMsg] = useState('');
+    const [color, setColor] = useState('');
+    const [afterSubmit, setAfterSubmit] = useState('hidden');
+    const [formVisibility, setFormVisibility] = useState('shown');
 
 
     const handleFormSubmit = async (e) => {
-        if(!Auth.loggedIn) {
+        if (!Auth.loggedIn) {
             console.log("User is not logged in")
         }
         try {
-        e.preventDefault();           
-        const mutationResponse = await addOrder({
-            variables: {
-                carYear: formState.carYear,
-                carMake: formState.carMake,
-                carModel: formState.carModel,
-                carColor: formState.carColor,
-                carPlate: formState.carPlate,
-                carVin: formState.carVin,
-                custName: formState.customerName,
-                custNumber: formState.customerNum,
-                custSt: formState.customerSt,
-                custCity: formState.customerCity,
-                custState: formState.customerState,
-                custZip: formState.customerZip,
-                orderSum: formState.orderSum,
-                orderEst: formState.orderEst
-            }
+            e.preventDefault();
+            const mutationResponse = await addOrder({
+                variables: {
+                    carYear: formState.carYear,
+                    carMake: formState.carMake,
+                    carModel: formState.carModel,
+                    carColor: formState.carColor,
+                    carPlate: formState.carPlate,
+                    carVin: formState.carVin,
+                    custName: formState.customerName,
+                    custNumber: formState.customerNum,
+                    custSt: formState.customerSt,
+                    custCity: formState.customerCity,
+                    custState: formState.customerState,
+                    custZip: formState.customerZip,
+                    orderSum: formState.orderSum,
+                    orderEst: formState.orderEst
+                }
 
-            /*variables: {
-                "carYear": "2016",
-                "carMake":"maserati",
-                "carModel":"ghibli",
-                "carColor":"black",
-                "carPlate":"plate12",
-                "carVin":"1G1PE5SB2E7134527",
-                "custName":"jason three",
-                "custNumber":"5101234567",
-                "custSt":"123 Street St",
-                "custCity":"City",
-                "custState":"State",
-                "custZip": "12345",
-                "orderSum":"sumarry foaisdjusiaas",
-                "orderEst": "123"
-            }*/
-        });
+                /*variables: {
+                    "carYear": "2016",
+                    "carMake":"maserati",
+                    "carModel":"ghibli",
+                    "carColor":"black",
+                    "carPlate":"plate12",
+                    "carVin":"1G1PE5SB2E7134527",
+                    "custName":"jason three",
+                    "custNumber":"5101234567",
+                    "custSt":"123 Street St",
+                    "custCity":"City",
+                    "custState":"State",
+                    "custZip": "12345",
+                    "orderSum":"sumarry foaisdjusiaas",
+                    "orderEst": "123"
+                }*/
+            });
 
-        setFormMsg('Info successfully sent');
+            setFormMsg('Info successfully sent');
+            setColor('green');
+            setAfterSubmit('');
+            setFormVisibility('hidden');
         } catch (error) {
             console.log(error);
             setFormMsg('There was a validation error in one field, please look over it again.');
+            setColor('red');
         }
     }
 
@@ -72,21 +81,21 @@ workSum: '', workEst: ''})
         });
     }
 
-    if(!Auth.loggedIn()) {
+    if (!Auth.loggedIn()) {
         return (
-            <div className="home" style={{display:'flex', justifyContent:'center', flexDirection:'column'}}>
-        <h3 style={{marginBottom:'0px'}}>Please log in to create a new form</h3>
-        <hr/>
-        <button className='loginBtn'><Link className='link loginLink' to="/login">Go to Login →</Link></button>
-        </div>
+            <div className="home" style={ { display: 'flex', justifyContent: 'center', flexDirection: 'column' } }>
+                <h3 style={ { marginBottom: '0px' } }>Please log in to create a new form</h3>
+                <hr />
+                <button className='loginBtn'><Link className='link loginLink' to="/login">Go to Login →</Link></button>
+            </div>
         )
     }
-    
+
     return (
         <div className='wrapper home'>
 
-            <h3>Create New Order</h3>
-            <form className='flex-row justify-center justify-space-between-md align-center' onSubmit={ handleFormSubmit }>
+            <h3>New Order</h3>
+            <form className={'flex-row justify-center justify-space-between-md align-center' + {formVisibility}} onSubmit={ handleFormSubmit }>
                 <div className='left'>
 
                     <div className='carDiv'>
@@ -94,34 +103,34 @@ workSum: '', workEst: ''})
                         <div className='carLeft'>
                             <div className='col-12'>
                                 <label>Year:
-                                    <input name="carYear" id="carYear" onChange={handleChange}/>
+                                    <input name="carYear" id="carYear" onChange={ handleChange } />
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>Make:
-                                    <input name="carMake" id='carMake' onChange={handleChange}/>
+                                    <input name="carMake" id='carMake' onChange={ handleChange } />
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>Model:
-                                    <input name="carModel" id="carModel" onChange={handleChange}/>
+                                    <input name="carModel" id="carModel" onChange={ handleChange } />
                                 </label>
                             </div>
                         </div>
                         <div className='carRight'>
                             <div className='col-12'>
                                 <label>Color:
-                                    <input name="carColor" id="carColor" onChange={handleChange}/>
+                                    <input name="carColor" id="carColor" onChange={ handleChange } />
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>License Plate #:
-                                    <input name="carPlate" id="carPlate" onChange={handleChange}/>
+                                    <input name="carPlate" id="carPlate" onChange={ handleChange } />
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>VIN #:
-                                    <input name="carVin" id="carVin" onChange={handleChange}/>
+                                    <input name="carVin" id="carVin" onChange={ handleChange } />
                                 </label>
                             </div>
                         </div>
@@ -132,34 +141,34 @@ workSum: '', workEst: ''})
                         <div className='customerLeft'>
                             <div className='col-12'>
                                 <label>Name:
-                                    <input name="customerName" id="customerName" onChange={handleChange}/>
+                                    <input name="customerName" id="customerName" onChange={ handleChange } />
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>Number:
-                                    <input name="customerNum" id="customerNum" onChange={handleChange}/>
+                                    <input name="customerNum" id="customerNum" onChange={ handleChange } />
                                 </label>
                             </div>
                         </div>
                         <div className='customerRight'>
                             <div className='col-12'>
                                 <label>Street:
-                                    <input name="customerSt" id="customerSt" onChange={handleChange}/>
+                                    <input name="customerSt" id="customerSt" onChange={ handleChange } />
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>City:
-                                    <input name="customerCity" id="customerCity" onChange={handleChange}/>
+                                    <input name="customerCity" id="customerCity" onChange={ handleChange } />
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>State:
-                                    <input name="customerState" id="customerState" onChange={handleChange}/>
+                                    <input name="customerState" id="customerState" onChange={ handleChange } />
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>ZIP:
-                                    <input name="customerZip" id="customerZip" onChange={handleChange}/>
+                                    <input name="customerZip" id="customerZip" onChange={ handleChange } />
                                 </label>
                             </div>
                         </div>
@@ -169,23 +178,24 @@ workSum: '', workEst: ''})
                         <div className='workLeft'>
                             <div className='col-12'>
                                 <label>Summary:
-                                    <textarea name="orderSum" id="orderSum" onChange={handleChange}/>
+                                    <textarea name="orderSum" id="orderSum" onChange={ handleChange } />
                                 </label>
                             </div>
                         </div>
                         <div className='workRight'>
                             <div className='col-12'>
                                 <label>Estimate:
-                                    <input name="orderEst" id="orderEst" onChange={handleChange}/>
+                                    <input name="orderEst" id="orderEst" onChange={ handleChange } />
                                 </label>
                             </div>
                         </div>
-                        <div>
+                        <div className='formSubmit'>
+                            <span className={color}>{ formMsg }</span>
+                            <span className={afterSubmit}>Click here to make another order.</span>
+                            <br/>
                             <button type='submit'>Submit</button>
+                            
                         </div>
-<div>
-    {formMsg}
-</div>
 
                     </div>
                 </div>
