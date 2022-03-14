fe77904ad2426ffa5e2ef87342775081bd9f9881
jsncorn <jsncourn@gmail.com>
2022-03-14 13:16:04 -0700

    fixed login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -45,10 +45,11 @@ const resolvers = {
          login: async(parent, { email, password }) => {
              const employee = await Employee.findOne({ email });
              if(!employee) {
+                 
                  throw new AuthenticationError('wrong login info');
              }
-
-             const correctPw = await employee.isCorrectPassword(password);
+            
+             const correctPw = await employee.isCorrectPassword(password); 
              if (!correctPw) {
                  throw new AuthenticationError('sadsadsad wrong password');
              }
