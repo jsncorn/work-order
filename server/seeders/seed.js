af14f137a6611f4baf920a629ba105164c29684d
jsncorn <jsncourn@gmail.com>
2022-03-14 13:58:46 -0700

    added inital admin account

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,10 +1,12 @@
 const db = require('../config/connection');
-const { Order } = require('../models');
+const { Order, Employee } = require('../models');
 const orderSeeds = require('./orderSeeds.json');
+const employeeSeeds = require('./employeeSeeds.json');
 
 db.once('open', async () => {
   await Order.deleteMany({});
   await Order.create(orderSeeds);
+  await Employee.create(employeeSeeds)
 
   console.log('all done!');
   process.exit(0);
