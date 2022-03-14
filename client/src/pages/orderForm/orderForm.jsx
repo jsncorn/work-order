4d5f579908cebf7de8a4f0236de873721daddbe0
jsncorn <jsncourn@gmail.com>
2022-03-14 10:41:20 -0700

    added handleformsubmit and handlechange for the form

--- client/src/pages/orderForm/orderForm.jsx
+++ client/src/pages/orderForm/orderForm.jsx
@@ -5,7 +5,7 @@ import { useMutation } from '@apollo/client';
 
 export default function Orderform(props) {
 
-    const [formState, setFormstate] = useState({ carYear: '', carMake: '',
+    const [formState, setFormState] = useState({ carYear: '', carMake: '',
 carModel: '', carColor: '', carPlate: '', carVin: '', customerName: '',
 customerNum: '', customerSt: '', customerCity: '', customerState: '', customerZip:'',
 workSum: '', workEst: ''})
@@ -13,9 +13,36 @@ workSum: '', workEst: ''})
 
     const handleFormSubmit = async (e) => {
         e.preventDefault();
-
+        const mutationResponse = await addOrder({
+            variables: {
+                carYear: formState.carYear,
+                carMake: formState.carMake,
+                carModel: formState.carModel,
+                carColor: formState.carColor,
+                carPlate: formState.carPlate,
+                carVin: formState.carVin,
+                customerName: formState.customerName,
+                customerSt: formState.customerSt,
+                customerCity: formState.customerCity,
+                customerState: formState.customerState,
+                customerZip: formState.customerZip,
+                workSum: formState.workSum,
+                workEst: formState.workEst
+            }
+        });
+        const token = mutationResponse.data.addOrder.token;
+        // no login for now
+        // Auth.login(token);
     }
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        })
+    }
+    
     return (
         <div className='wrapper'>
 
@@ -28,34 +55,34 @@ workSum: '', workEst: ''})
                         <div className='carLeft'>
                             <div className='col-12'>
                                 <label>Year:
-                                    <input name="carYear" />
+                                    <input name="carYear" onChange={handleChange}/>
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>Make:
-                                    <input name="carMake" />
+                                    <input name="carMake" onChange={handleChange}/>
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>Model:
-                                    <input name="carModel" />
+                                    <input name="carModel" onChange={handleChange}/>
                                 </label>
                             </div>
                         </div>
                         <div className='carRight'>
                             <div className='col-12'>
                                 <label>Color:
-                                    <input name="carColor" />
+                                    <input name="carColor" onChange={handleChange}/>
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>License Plate #:
-                                    <input name="carPlate" />
+                                    <input name="carPlate" onChange={handleChange}/>
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>VIN #:
-                                    <input name="carVin" />
+                                    <input name="carVin" onChange={handleChange}/>
                                 </label>
                             </div>
                         </div>
@@ -66,34 +93,34 @@ workSum: '', workEst: ''})
                         <div className='customerLeft'>
                             <div className='col-12'>
                                 <label>Name:
-                                    <input name="customerName" />
+                                    <input name="customerName" onChange={handleChange}/>
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>Number:
-                                    <input name="customerNum" />
+                                    <input name="customerNum" onChange={handleChange}/>
                                 </label>
                             </div>
                         </div>
                         <div className='customerRight'>
                             <div className='col-12'>
                                 <label>Street:
-                                    <input name="customerSt" />
+                                    <input name="customerSt" onChange={handleChange}/>
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>City:
-                                    <input name="customerCity" />
+                                    <input name="customerCity" onChange={handleChange}/>
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>State:
-                                    <input name="customerState" />
+                                    <input name="customerState" onChange={handleChange}/>
                                 </label>
                             </div>
                             <div className='col-12'>
                                 <label>ZIP:
-                                    <input name="customerZip" />
+                                    <input name="customerZip" onChange={handleChange}/>
                                 </label>
                             </div>
                         </div>
@@ -103,14 +130,14 @@ workSum: '', workEst: ''})
                         <div className='workLeft'>
                             <div className='col-12'>
                                 <label>Summary:
-                                    <textarea name="workSum"  />
+                                    <textarea name="workSum"  onChange={handleChange}/>
                                 </label>
                             </div>
                         </div>
                         <div className='workRight'>
                             <div className='col-12'>
                                 <label>Estimate:
-                                    <input name="workEst" />
+                                    <input name="workEst" onChange={handleChange}/>
                                 </label>
                             </div>
                         </div>
