96eaee0c0fc043a66635c5980285d80ea7a956c4
jsncorn <jsncourn@gmail.com>
2022-03-14 13:53:46 -0700

    show logout if logged in, show create account if logged in, and show login if not signed in

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./menu.scss";
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/Auth';
+
 
 export default function Menu({ menuOpen, setMenuOpen }) {
+
+
+const [logged, setInfo] = useState('true')
+const [logout, setLogout] = useState('hidden');
+const checkLoggedIn = () => {
+    if(Auth.loggedIn()) {
+        setInfo('hidden');
+        setLogout('true');
+    }
+
+};
+
+useEffect(() => {
+    checkLoggedIn()
+})
+
     return (
         <div className={"navMenu " + (menuOpen && "active")}>
             <ul>
                 <li onClick={() => setMenuOpen(false)}>
-                    <Link to='/'>Home</Link>
+                    <Link to='/'>Work Orders</Link>
                 </li>
                 <li onClick={() => setMenuOpen(false)}>
-                    <Link to='/newform'>Create</Link>
+                    <Link to='/newform'>New Order</Link>
                 </li>
-                <li onClick={() => setMenuOpen(false)}>
+                <li className={ logged } onClick={() => setMenuOpen(false)}>
                     <Link to='/login'>Login</Link>
                 </li>
-                <li onClick={() => setMenuOpen(false)}>
-                    <Link to='/signup'>Signup</Link>
+                <li className={logout} onClick={() => setMenuOpen(false)}>
+                    <Link to='/signup'>Create Account</Link>
+                </li>
+                <li className={logout} onClick={() => { 
+                    setMenuOpen(false);
+                    Auth.logout();
+                    console.log('logged out.')
+                }}>
+                    <a href='/'>Logout</a>
                 </li>
             </ul>
         </div>
